55b142db050c04961c6c628960022f970f9aee7d
SoulQrat <fedor-890@mail.ru>
2025-11-14 01:49:00 +0300

    add collect_stats atribute

--- src/train/train.py
+++ src/train/train.py
@@ -13,6 +13,7 @@ def train_epoch(
     device,
     scheduler=None,
     experiment=None,
+    collect_stats=True,
 ):
     model.train()
     total_loss = 0
@@ -25,7 +26,7 @@ def train_epoch(
         mask = mask.to(device)
         optimizer.zero_grad()
 
-        if torch.cuda.is_available():
+        if collect_stats and torch.cuda.is_available():
             torch.cuda.synchronize()
             torch.cuda.reset_peak_memory_stats()
 
@@ -33,7 +34,7 @@ def train_epoch(
         outputs = model(input_ids=data, attention_mask=mask, labels=data)
         t_forward = time.perf_counter() - t_forward
 
-        if torch.cuda.is_available():
+        if collect_stats and torch.cuda.is_available():
             torch.cuda.synchronize()
             memory = torch.cuda.max_memory_allocated()
 
@@ -51,24 +52,28 @@ def train_epoch(
         total_t_backward += t_backward
         if experiment is not None:
             experiment.log_metric("loss", loss.item(), step=epoch * len(dataloader) + i)
-            experiment.log_metric(
-                "forward_time", t_forward, step=epoch * len(dataloader) + i
-            )
-            experiment.log_metric(
-                "backward_time", t_backward, step=epoch * len(dataloader) + i
-            )
-            if torch.cuda.is_available():
+            if collect_stats:
                 experiment.log_metric(
-                    "memory", memory, step=epoch * len(dataloader) + i
+                    "forward_time", t_forward, step=epoch * len(dataloader) + i
                 )
+                experiment.log_metric(
+                    "backward_time", t_backward, step=epoch * len(dataloader) + i
+                )
+                if torch.cuda.is_available():
+                    experiment.log_metric(
+                        "memory", memory, step=epoch * len(dataloader) + i
+                    )
 
     if experiment is not None:
         experiment.log_metric(
             "epoch_loss", total_loss / len(dataloader), step=epoch + 1
         )
-        experiment.log_metric(
-            "epoch_forward_time", total_t_forward / len(dataloader), step=epoch + 1
-        )
-        experiment.log_metric(
-            "epoch_backward_time", total_t_backward / len(dataloader), step=epoch + 1
-        )
+        if collect_stats:
+            experiment.log_metric(
+                "epoch_forward_time", total_t_forward / len(dataloader), step=epoch + 1
+            )
+            experiment.log_metric(
+                "epoch_backward_time",
+                total_t_backward / len(dataloader),
+                step=epoch + 1,
+            )
